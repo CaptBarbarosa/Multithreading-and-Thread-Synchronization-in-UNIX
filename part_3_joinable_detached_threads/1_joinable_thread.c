a926e50388d7eb4c2705d4c5d6590ea4c8402842
CaptBarbarosa <tandogankaan5@gmail.com>
2024-07-15 20:44:04 +0100

    Joinable Editing

--- part_3_joinable_detached_threads/1_joinable_thread.c
+++ part_3_joinable_detached_threads/1_joinable_thread.c
@@ -12,21 +12,24 @@ void* child_thread_function(void* arg) {
     pthread_exit(NULL);
 }
 
-int main() {
+int main(){
     pthread_t child_thread;
-
-    if (pthread_create(&child_thread, NULL, child_thread_function, NULL) != 0) {
-        fprintf(stderr, "Error creating thread\n");
-        return 1;
+    pthread_attr_t attr;
+    if (pthread_attr_init(&attr)!=0){ //Initialized the attribute
+        printf("Failed to initialize attribute");
     }
+    if(pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE)!=0){ // Made it joinable
+        printf("Failed to set it joinable");
+    }
+    if (pthread_create(&child_thread, &attr, child_thread_function, NULL) != 0){ //Threads are joinable by default actually.
+        printf(s"Failed to create thread\n");
+        return 1;}
 
     printf("Parent thread is doing its thing\n"); // Perform some tasks
-
-    if (pthread_join(child_thread, NULL) != 0) { // Wait for child to terminate and join it
-        fprintf(stderr, "Error joining thread\n");
+    if (pthread_join(child_thread, NULL) != 0) { // Wait for child to terminate and join.
+        printf("Failed to join threads\n");
         return 1;
     }
-
     printf("Child thread has terminated and resources are released\n");
     return 0;
 }
