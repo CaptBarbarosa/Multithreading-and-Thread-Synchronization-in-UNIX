869161d5e0e559dd5d5ca95a15ce73ada723091e
CaptBarbarosa <tandogankaan5@gmail.com>
2024-07-17 21:32:19 +0100

    end of day update

--- part_0_Glue_Based_Linked_List/glthread.h
+++ part_0_Glue_Based_Linked_List/glthread.h
@@ -1,28 +1,43 @@
 #ifndef __GLTHREADS__
-#define __GLTHREADS_
+#define __GLTHREADS__
 
+#include <stddef.h> // Include for size_t
+
+// Define the glthread_node_ structure to be used as a node in the glthread list
 typedef struct glthread_node_ {
-    // Please recognize that there is no data value unlike traditional doubly linked lists.
-    struct glthread_node_ *left;
-    struct glthread_node_ *right;
-}glthread_node_t;
+    struct glthread_node_ *left;  // Pointer to the previous node in the list
+    struct glthread_node_ *right; // Pointer to the next node in the list
+} glthread_node_t;
 
-typedef struct gldll_ {
-    glthread_node_t *head;
-    int (*key_match)(void *, void *); 
-    /* Func. pointer to a key_match function. Takes 2 void arguments and returns an 'int'. Please note that the void pointers allow for flexibility in the type of data stored in the list nodes.*/
-    int (*comparison_fn)(void *, void *); 
-    unsigned int offset;
-}gldll_t;
+// Define the glthread_ structure to represent the glthread list
+typedef struct glthread_ {
+    glthread_node_t *head;  // Pointer to the head node of the list
+    size_t offset;    // Offset of the glthread node in the application-defined data structure
+} glthread_t;
 
-/* 
-   In the traditional doubly linked lists we'd have a void *data. Te void pointer is for efficiency and as you can observe we don't have a void pointer in the gll. But then, how is the glued data structure holds the data?
-   The traditional d.l.l
-*/
+// Function to add a new node to the glthread list
+void glthread_add(glthread_t *lst, glthread_node_t *glnode);
 
+// Function to remove a node from the glthread list
+void glthread_remove(glthread_t *lst, glthread_node_t *glnode);
 
+// Macro to iterate over all nodes in a glthread list
+#define ITERATE_GL_THREADS_BEGIN(lstptr, struct_type, ptr)      \
+{                                                               \
+    glthread_node_t *_glnode = NULL, *_next = NULL;             \
+    for(_glnode = (lstptr)->head; _glnode; _glnode = _next){    \
+        _next = _glnode->right;                                 \
+        ptr = (struct_type *)((char *)_glnode - (lstptr)->offset);
 
+#define ITERATE_GL_THREADS_ENDS }}
 
+// Macro to initialize a glthread node
+#define glthread_node_init(glnode)  \
+    (glnode)->left = NULL;          \
+    (glnode)->right = NULL;
 
+// Function to initialize a glthread list
+void init_glthread(glthread_t *glthread, size_t offset);
 
-#endif
+#endif /* __GLTHREADS__ */
+
--- part_0_Glue_Based_Linked_List/main.c
+++ part_0_Glue_Based_Linked_List/main.c
@@ -1,10 +1,49 @@
-#The GL at the beginning means "glued" and the reason why it is called thread is that it is a linked list and linked lists are like threads. Single and long.
+#include "glthread.c"
+#include <stdlib.h>
+#include <stdio.h>
 
-#This "glue" concept is also aplicable to other data structures like trees, stacks etc. but here we will use doubly linked lists.
+// Define an application-specific structure
+typedef struct person_ {
+    char name[30];
+    int age;
+    glthread_node_t glnode; // Glue thread node
+} person_t;
 
-#include<stdio.h>
+int main() {
+    // Create and initialize the glthread list
+    glthread_t list;
+    init_glthread(&list, offsetof(person_t, glnode));
 
+    // Create and initialize some person structures
+    person_t person1 = {.name = "Alice", .age = 25};
+    person_t person2 = {.name = "Bob", .age = 30};
+    person_t person3 = {.name = "Charlie", .age = 35};
 
-int main(){
+    // Initialize the glthread nodes within the person structures
+    glthread_node_init(&person1.glnode);
+    glthread_node_init(&person2.glnode);
+    glthread_node_init(&person3.glnode);
+
+    // Add the person structures to the glthread list
+    glthread_add(&list, &person1.glnode);
+    glthread_add(&list, &person2.glnode);
+    glthread_add(&list, &person3.glnode);
+
+    // Iterate over the list and print the persons
+    person_t *person_ptr;
+    ITERATE_GL_THREADS_BEGIN(&list, person_t, person_ptr) {
+        printf("Name: %s, Age: %d\n", person_ptr->name, person_ptr->age);
+    } ITERATE_GL_THREADS_ENDS;
+
+    // Remove a person from the list
+    glthread_remove(&list, &person2.glnode);
+
+    // Iterate over the list again and print the remaining persons
+    printf("\nAfter removal:\n");
+    ITERATE_GL_THREADS_BEGIN(&list, person_t, person_ptr) {
+        printf("Name: %s, Age: %d\n", person_ptr->name, person_ptr->age);
+    } ITERATE_GL_THREADS_ENDS;
+
     return 0;
 }
+
