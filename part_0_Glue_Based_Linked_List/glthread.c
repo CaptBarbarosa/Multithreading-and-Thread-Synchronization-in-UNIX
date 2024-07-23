099c7cc4935fc5a0d37693a216d301dd036fc92a
CaptBarbarosa <tandogankaan5@gmail.com>
2024-07-23 19:02:38 +0100

    NFC start

--- part_0_Glue_Based_Linked_List/glthread.c
+++ part_0_Glue_Based_Linked_List/glthread.c
@@ -4,54 +4,46 @@
 
 // Private helper function to add a new_node right after curr_node in the thread
 static void _glthread_add_next(glthread_node_t *curr_node, glthread_node_t *new_node){
-
     // If the current node's right is NULL, insert new_node to the right of it
     if(!curr_node->right){
         curr_node->right = new_node;
         new_node->left = curr_node;
         return;
     }
-
     // Save the node originally to the right of curr_node
     glthread_node_t *temp = curr_node->right;
-    
     // Update the right of curr_node to point to new_node
     curr_node->right = new_node;
-    
     // Update new_node's left and right pointers
     new_node->left = curr_node;
     new_node->right = temp;
-    
     // Update the 'left' of the node originally to the right of curr_node
     temp->left = new_node;
 }
 
+
 // Public API function to add a node to the beginning of a GL thread
 void glthread_add (glthread_t *lst, glthread_node_t *glnode){
-
     // Initialize the left and right pointers of the new node to NULL
     glnode->left = NULL;
     glnode->right = NULL;
-
     // If the list is empty, set the head to the new node
     if(!lst->head){
         lst->head = glnode;
         return;
     }
-    
     // Save current head node
     glthread_node_t *head = lst->head;
-    
     // Insert new_node before current head
     _glthread_add_next(glnode, head);
-    
     // Update the head of the list to point to the new node
     lst->head = glnode;
 }
 
+
+
 // Private helper function to remove a node from a GL thread
 static void _remove_glthread(glthread_node_t *glnode){
-
     // Case 1: Node to be removed is at the beginning
     if(!glnode->left){
         if(glnode->right){
@@ -61,7 +53,6 @@ static void _remove_glthread(glthread_node_t *glnode){
         }
         return;
     }
-    
     // Case 2: Node to be removed is at the end
     if(!glnode->right){
         glnode->left->right = NULL;
---
+++ part_6_Notification_Chains/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+/*
+    Notification chains is an architectual concept used to notify multiple subscribers interested in the particular event
+*/
+int main(){
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
