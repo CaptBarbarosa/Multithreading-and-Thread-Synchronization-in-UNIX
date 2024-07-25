3516d05de3a715c0a33b3bc85235eab03a6d3b65
CaptBarbarosa <tandogankaan5@gmail.com>
2024-07-25 17:42:08 +0100

    thread_conditions

--- part_0_pthread_conditions/example_1.c
+++ part_0_pthread_conditions/example_1.c
@@ -1,58 +1,38 @@
-#include <pthread.h>
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <pthread.h>
 #include <unistd.h>
-
-pthread_mutex_t mutex_1;
-int condition=0;
-
-void *waiting_thread(void *arg){
-    int i;
-    for (i=0; i<5;i++){
-        pthread_mutex_lock(&mutex_1);
-        condition++;
-        //printf("->Condition is: %d\n",condition);
-        pthread_mutex_unlock(&mutex_1);
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+int condition = 0;
+void* waiting_thread(void* arg) {
+    pthread_mutex_lock(&mutex);
+    while (condition == 0) {
+        printf("Waiting for the condition to be met...\n");
+        pthread_cond_wait(&cond, &mutex);
     }
+    printf("Condition met, continuing execution...\n");
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
-
-
-void *signalling_thread(void *arg){
-    {
-        pthread_mutex_lock(&mutex_1);
-        condition--;
-        //printf("-->Condition is: %d\n",condition);
-        pthread_mutex_unlock(&mutex_1);
-    }
+void* signaling_thread(void* arg) {
+    sleep(2);  
+    pthread_mutex_lock(&mutex);
+    condition = 1;
+    printf("Condition met, signaling waiting thread...\n");
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
-
-int main(){
-    int i;
-    pthread_mutex_init(&mutex_1, NULL);
-    pthread_t my_threads[2];
-    for(i=0;i<2;i++){
-        if(i==1){
-            if(pthread_create(&my_threads[0], NULL, waiting_thread, NULL) != 0){
-                perror("Failed to create first thread\n");
-                exit(1);
-            }
-        }
-        
-        else{
-            if(pthread_create(&my_threads[1], NULL, signalling_thread, NULL)){
-                perror("Failed to create second thread\n");
-                exit(1);
-            }
-        }
-    }
-    for(i=0; i<2; i++){
-        if(pthread_join(my_threads[i], NULL) != 0){
-            perror("Failed to join\n");
-            exit(1);
-        }
-    }
+int main() {
+    pthread_t t1, t2;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
+    pthread_create(&t1, NULL, waiting_thread, NULL);
+    pthread_create(&t2, NULL, signaling_thread, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond);
     return 0;
 }
