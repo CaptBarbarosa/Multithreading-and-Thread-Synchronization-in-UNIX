10e937aa3a99a49d727d974a9edb0dd3c1baa7c4
CaptBarbarosa <tandogankaan5@gmail.com>
2024-07-25 21:21:22 +0100

    Asyn Cancellation

--- part_7_asynchronous_thread_cancellation/main.c
+++ part_7_asynchronous_thread_cancellation/main.c
@@ -1,7 +1,13 @@
 #include "glthread.c"
 #include <stdlib.h>
 #include <stdio.h>
-//When a thread tries to cancel another threat, the cancellation request (CR) is queued by OS.
+// Now assume that we have two threads. thread_1 and thread_2. They both are in operation and thread_1 tries to cancel thread_2. The thread_1 sends a cancellation request [CR(pthread_cancel)].
+
+// When a thread tries to cancel another threat, the cancellation request [CR(pthread_cancel())] is queued by OS. It isn't delivered immediately.
+
+// The OS afterwards, looks for an opportunity to terminate thread T2.
+
+// OS may not terminate the thread instantly (Asynchronous).
 
 int main(){
     return 0;
